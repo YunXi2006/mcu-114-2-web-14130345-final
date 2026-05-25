7597d32c2ef6c57147a7a26dbbc1eadf5ca97225
yunxi <yunxi@activekm.com>
2026-05-25 22:48:59 +0800

    feat(product): 建立產品卡片清單元件

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,10 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
 import { booleanAttribute, Component, EventEmitter, Input, numberAttribute, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
+  host: { class: 'app-product-card' },
 })
 export class ProductCardComponent {
   @Input({ required: true, transform: numberAttribute })
