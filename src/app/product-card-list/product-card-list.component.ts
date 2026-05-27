4b9e3a262de14f4e5ad7ff59d6c7299de13ca73c
yunxi <yunxi@activekm.com>
2026-05-28 00:22:02 +0800

    feat(product-card-list): 產品查詢功能實作

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,21 +1,31 @@
-import { Component, input, output, signal } from '@angular/core';
-
+import { Component, input, output, signal, computed, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../model/product';
 import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-card-list',
-  imports: [PaginationComponent, ProductCardComponent],
+  imports: [FormsModule, PaginationComponent, ProductCardComponent],
   templateUrl: './product-card-list.component.html',
   styleUrl: './product-card-list.component.scss',
 })
-export class ProductCardListComponent {
+export class ProductCardListComponent implements OnInit {
   readonly products = input<Product[]>([]);
 
   readonly edit = output<Product>();
-
   readonly remove = output<Product>();
-
   readonly view = output<Product>();
+
+  searchKeyword = '';
+  filteredProducts = signal<Product[]>([]);
+
+  ngOnInit() {
+    this.filteredProducts.set(this.products());
+  }
+
+  onSearch() {
+    const keyword = this.searchKeyword.trim();
+    this.filteredProducts.set(this.products().filter((p) => p.name.includes(keyword)));
+  }
 }
