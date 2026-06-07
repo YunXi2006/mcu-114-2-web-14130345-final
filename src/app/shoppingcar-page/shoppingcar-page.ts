68fb12275e40d87a7741c9bef67dc84bd83b7ede
yunxi <yunxi@activekm.com>
2026-06-07 21:40:18 +0800

    feat(shoppingcar-page): 實作產品作者表單輸入

--- src/app/shoppingcar-page/shoppingcar-page.ts
+++ src/app/shoppingcar-page/shoppingcar-page.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shoppingcar-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './shoppingcar-page.component.html',
   styleUrl: './shoppingcar-page.component.scss',
 })
-export class ShoppingCarPageComponent {}
+export class ShoppingCarPageComponent {
+  protected readonly form = new FormGroup({
+    name: new FormControl<string | null>(null),
+    address: new FormControl<string | null>(null),
+    phone: new FormControl<string | null>(null),
+  });
+}
